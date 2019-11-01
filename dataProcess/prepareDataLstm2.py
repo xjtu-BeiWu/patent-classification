3fc12a2c496d7da9e17affc61980868785859f10
xjtu-BeiWu <xjtu_BeiWu@163.com>
2019-11-01 16:35:52 +0800

    change word2vec test code

--- dataProcess/prepareDataLstm2.py
+++ dataProcess/prepareDataLstm2.py
@@ -1,8 +1,20 @@
 from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 
-model = Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
-#the model is loaded. It can be used to perform all of the tasks mentioned above.
+# the model is loaded. It can be used to perform all of the tasks mentioned above.
 
 dog = model['dog']
-print(model.similarity('woman', 'man'))
+print(model.similarity('8', 'eight'))
+
+# from gensim.test.utils import common_texts, get_tmpfile
+# from gensim.models import Word2Vec
+#
+# path = get_tmpfile("word2vec.model")
+# model = Word2Vec(common_texts, size=100, window=5, min_count=1, workers=4)
+# model.save("word2vec.model")
+# model = Word2Vec.load("word2vec.model")
+#
+# vector1 = model.wv['nine']
+# vector2 = model.wv['eight']
